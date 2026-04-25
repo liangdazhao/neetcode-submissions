68b9ca1c846de045b78d6b1b4a7cfcb73d0f7f47
Liangda Zhao <121745145+liangdazhao@users.noreply.github.com>
2026-04-25 16:47:17 -0400

    Add: asteroid-collision - submission-8

---
+++ Data Structures & Algorithms/asteroid-collision/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        
+        for a in asteroids:
+            alive = True
+            
+            while stack and a < 0 and stack[-1] > 0:
+                if stack[-1] > abs(a):
+                    alive=False
+                    break
+                elif stack[-1] < abs(a):
+                    stack.pop()
+                else:
+                    stack.pop()
+                    alive=False
+                    break
+            
+            if alive:
+                stack.append(a)
+
+        return stack
+            
