04ce68fbf8829729250de824a006682aaa358699
Liangda Zhao <121745145+liangdazhao@users.noreply.github.com>
2026-04-25 10:52:16 -0400

    Add: sqrtx - submission-12

---
+++ Data Structures & Algorithms/sqrtx/submission-12.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+
+        res=1
+        l, r = 0, x
+
+        while l<=r:
+            m=(l+r)//2
+            if m*m <= x:
+                res=m
+                l=m+1
+            else:
+                r=m-1
+        return res
+
